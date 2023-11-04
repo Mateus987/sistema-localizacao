525f6d0fac5581f8cfa2144243cb3e9a716edf2c
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-11-04 14:25:04 -0300

    Adicionado lógica do metodo get_elements_in_queue(método que pega todos os valores da fila, um por um) / Iniciado a criação da lógica de threads (tres threads devem ser criados: um para o websocket, outro para a api e outro para o consumidor da fila, ou seja o get_elements_in_queue)

--- System 2/app.py
+++ System 2/app.py
@@ -1,10 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 from flask_socketio import SocketIO, emit
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 import json
 import uuid
+import pika
+import time
+import threading
 
 app = Flask(__name__)
 api = Api(app)
@@ -86,14 +89,6 @@ api.add_resource(DispositivoResource, '/dispositivo/<string:dispositivo_id>')
 api.add_resource(HistoricoLocalizacaoResource, '/historico/<string:dispositivo_id>')
 
 
-@socketio.on('connect')
-def handle_connect():
-    print('Cliente WebSocket conectado')
-
-@socketio.on('disconnect')
-def handle_disconnect():
-    print('Cliente WebSocket desconectado')
-
 def enviar_ultima_localizacao(dispositivo_id):
     localizacao = Localizacao.query.filter_by(id_dispositivo=dispositivo_id).order_by(Localizacao.id.desc()).first()
     if localizacao:
@@ -111,15 +106,35 @@ def atualizar_localizacao_em_tempo_real():
             enviar_ultima_localizacao(dispositivo_id)
         time.sleep(1)
 
-if __name__ == '__main__':
-    from threading import Thread
+@app.route("/")
+def raiz():
+    return make_response(jsonify({"Mensagem" : "Api esta funcionando!"})), 200
 
-    with app.app_context():
-        socket_thread = Thread(target=atualizar_localizacao_em_tempo_real)
-        socket_thread.start()
+def get_elements_in_queue(ch, method, properties, body):
+    element = json.loads(body.decode("utf-8"))
+    # TODO: criar a logica para inserir o element dentro do banco de dados
+    print(f"Queue: {method.routing_key}, Mensagem: {element['longitude']}")
 
-        socketio.run(app)
+def consume_queue():
+    connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+    channel = connection.channel()
+    queue_name = "localizacao_queue"
+    channel.queue_declare(queue=queue_name)
+    channel.basic_consume(queue=queue_name, on_message_callback=get_elements_in_queue, auto_ack=True)
+    channel.start_consuming()
 
+def init_server():
+    app.run("127.0.0.1", 3333, False)
+
+thread1 = threading.Thread(target=init_server)
+thread2 = threading.Thread(target=consume_queue)
+
+if __name__ == "__main__":
+    # TODO: melhorar esse sistema de threads, pois foi improvissado para que ambos os sistemas rodassem juntos
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
 """
 API de Rastreio
 - Um banco de dados
